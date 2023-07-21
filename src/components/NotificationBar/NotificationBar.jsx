ab2eacd47504f8e7729a1954b70e7bafd90c48a5
Chibuzo Ekwue <chieezy5@gmail.com>
2023-07-21 23:00:22 +0100

    the notification bar component

---
+++ src/components/NotificationBar/NotificationBar.jsx
@@ -0,0 +1,12 @@
+import "./NotificationBar.scss";
+
+const NotificationBar = () => {
+    return (
+			<div className="containter notification">
+				Use the promo code <span>MYKICKS</span> to get $10 off your next
+				purchase
+			</div>
+		);
+}
+ 
+export default NotificationBar;
