6b0c364906e7d40a6db664000f4ebaa983f9e8a1
Chibuzo Ekwue <chieezy5@gmail.com>
2023-07-22 23:08:58 +0100

    added shared components, added font and toaster container and css

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,12 @@
-//import './globals.css'
-import { Inter } from 'next/font/google'
+import NotificationBar from '@/components/NotificationBar/NotificationBar';
+import NavBar from '@/components/NavBar/NavBar';
+import './globals.scss';
+import { Open_Sans } from "next/font/google";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const inter = Inter({ subsets: ['latin'] })
+
+const openSans = Open_Sans({ subsets: ["latin"] });
 
 export const metadata = {
 	title: "Kicks - Step into style : your perfect fit awaits",
@@ -10,8 +15,24 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+		<html lang="en">
+			<body className={openSans.className}>
+				<ToastContainer
+					position="top-right"
+					autoClose={5000}
+					hideProgressBar={false}
+					newestOnTop
+					closeOnClick
+					rtl={false}
+					pauseOnFocusLoss
+					draggable
+					pauseOnHover
+					theme="light"
+				/>
+				<NotificationBar />
+				<NavBar />
+				{children}
+			</body>
+		</html>
+	);
 }
