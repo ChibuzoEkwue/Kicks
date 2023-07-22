8e8c9b4fda88d72b349fa990bc2a21ada2b42cd4
Chibuzo Ekwue <chieezy5@gmail.com>
2023-07-22 23:04:47 +0100

    Products page

---
+++ src/app/products/page.jsx
@@ -0,0 +1,11 @@
+import Products from "@/components/Products/Products";
+
+const ProductsPage = () => {
+	return (
+		<section className="container">
+			<Products /> 
+		</section>
+	);
+};
+
+export default ProductsPage;
