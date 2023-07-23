221c3f5baf5653d9901a9375eb1f4d5eb2764e22
Chibuzo Ekwue <chieezy5@gmail.com>
2023-07-23 14:18:06 +0100

    The wishlist page

---
+++ src/app/wishlist/page.jsx
@@ -0,0 +1,15 @@
+import "./styles.scss";
+import Link from "next/link";
+const WishList = () => {
+	return (
+		<section className="wishlist">
+			<div className="container">
+				<div className="emptyCard">
+					<p>No items added</p>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default WishList;
